23ad0e7987965707f72dbb82ecaf2a355c5164af
SelectSoft <anonymous55215@gmail.com>
2020-12-22 14:07:34 +0500

    material use

--- src/components/construction/elements/materialUse.js
+++ src/components/construction/elements/materialUse.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-const MaterialUse = () => {
+import Dropdown from 'react-bootstrap/Dropdown'
+const MaterialUse = (props) => {
   return (
     <React.Fragment>
       <div className="container" style={{ width: "100%", margin: "0px" }}>
@@ -11,27 +12,18 @@ const MaterialUse = () => {
             <p style={{ fontSize: "25px" }}>Type of material: </p>
           </div>
 
-          <div class="dropdown col-md-2" style={{ width: "100%" }}>
-            <br />
-            <button
-              class="btn dropdown-toggle"
-              type="button"
-              data-toggle="dropdown"
-              style={{ border: "1px solid black" }}
-            >
-              Construction
-            </button>
-            <ul class="dropdown-menu">
-              <li>
-                <a >HTML</a>
-              </li>
-              <li>
-                <a >CSS</a>
-              </li>
-              <li>
-                <a >JavaScript</a>
-              </li>
-            </ul>
+          <div class="dropdown col-md-3" style={{ marginTop: "20px" }}>
+            <Dropdown onSelect={props.handleDropdown}>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Construction
+                  </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item eventKey="action">Action</Dropdown.Item>
+                <Dropdown.Item eventKey="another" >Another action</Dropdown.Item>
+                <Dropdown.Item eventKey="else">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </div>
 
           <div class="dropdown col-md-3">

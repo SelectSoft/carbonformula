712bf977ced49d854e623ece2adccf3aa17ceb89
zainnasir138 <zainnasir138@gmail.com>
2021-03-04 11:16:40 +0500

    task 1,3,4 done

--- src/components/viewEmissions/viewemissions.js
+++ src/components/viewEmissions/viewemissions.js
@@ -8,7 +8,6 @@ import classes from "../../style/building.module.css";
 class ViewEmissions extends Component {
   constructor(props) {
     super(props);
-    console.log(props);
     this.state = {
       businessOperations: null,
       sites: null,
